9261f3b364ecb8111e2cb6052f9345cd40f72f5b
cr4dle <cr4dle@gmail.com>
2016-08-09 20:26:07 +0100

    Add Webp API 2 controller

---
+++ CashMachine/Controllers/WithdrawController.cs
@@ -0,0 +1,26 @@
+﻿using CashMachine.DTO;
+using CashMachine.Interfaces;
+using System;
+using System.Web.Http;
+
+namespace CashMachine.Controllers
+{
+    public class WithdrawController : ApiController
+    {
+        private ICashMachineService _cashMachineService;
+
+        public WithdrawController(ICashMachineService cashMachineService)
+        {
+            if (cashMachineService == null) throw new ArgumentNullException(nameof(cashMachineService));
+
+            _cashMachineService = cashMachineService;
+        }
+
+        // GET: api/Withdraw/120
+        public WithdrawDTO Get(double quantity)
+        {
+            var debug = _cashMachineService.Withdraw(quantity);
+            return debug;
+        }
+    }
+}
