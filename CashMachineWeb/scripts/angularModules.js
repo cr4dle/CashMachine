4852c08022af5b93656239dd31b22391cea40759
cr4dle <cr4dle@gmail.com>
2016-08-09 21:53:07 +0100

    Setup angular and create a directive to show the withdraw information

--- CashMachineWeb/scripts/angularModules.js
+++ CashMachineWeb/scripts/angularModules.js
@@ -1,3 +1,35 @@
 angular.module('init', [])
 .controller('ckCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    // Default value to withdraw
+    $scope.amount = 120;
+
+    $scope.withdraw = function () {
+        $http.get('http://localhost:14926/api/Withdraw/' + $scope.amount)
+        .then(function mySucces(response) {
+            let result = response.data;
+
+            if (result !== undefined && !result.EnoughCash) {
+                alert('There is no enough cash to withdraw');
+            } else {
+                $scope.balance = result.Balance;
+                $scope.withdrawDetails = result.Withdraw;
+            }
+        }, function myError(response) {
+            alert('Server error: ' + response.statusText);
+        });
+    };
+}])
+.directive('ckWithdraw', function () {
+    return {
+        templateUrl: './templates/withdraw.html',
+        restrict: 'E',
+        scope: {
+            balance: '=balance',
+            withdrawDetails: '=withdrawDetails'
+        },
+        controller: ['$scope', '$http', function ($scope, $http) {
+            
+        }]
+    };
 });
