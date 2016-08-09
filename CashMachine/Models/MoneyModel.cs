df0d2225c85a5ebf075457d1771fead03a4024b7
cr4dle <cr4dle@gmail.com>
2016-08-09 19:12:27 +0100

    Create Money model

---
+++ CashMachine/Models/MoneyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CashMachine.Models
+{
+    public class MoneyModel
+    {
+        public bool isNote { get; set; }
+
+        public string Name { get; set; }
+
+        public double Value { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
