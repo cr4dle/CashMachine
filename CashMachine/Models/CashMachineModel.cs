94b556c98c3b35da1eeeb6fab2953eb412e4c870
cr4dle <cr4dle@gmail.com>
2016-08-09 19:12:58 +0100

    Create CashMachine model

---
+++ CashMachine/Models/CashMachineModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CashMachine.Models
+{
+    public class CashMachineModel
+    {
+        public string CurrencySymbol { get; set; }
+
+        public bool isRight { get; set; }
+
+        public List<MoneyModel> Money { get; set; }
+    }
+}
